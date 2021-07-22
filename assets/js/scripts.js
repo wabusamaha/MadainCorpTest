dbae4f1b2de85fbe0c7f375bb6e9d825f56cf8ee
wabusamaha <52813165+wabusamaha@users.noreply.github.com>
2021-07-22 14:41:27 +0300

    Progress 0.02

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,5 @@
 const bodyPosition = ["Face", "Belly", "Back", "Tail"];
+const bodyPositionPoints = [10, 7, 5, 3];
 
 class Contender{
     constructor(type){
@@ -9,12 +10,15 @@ class Contender{
     }
 
     setType(type){
+        debugger;
         this.type = type;
     }
 
     changePosition(){
        this.position = getRandomInt(3);
-       console.log(bodyPosition[this.position]);
+    //    console.log(bodyPosition[this.position]);
+       console.log(this.type + ' position is "' + bodyPosition[this.position] + '"');
+
     //    this.postionTimer = setTimeout(this.changePosition, 1000);
     }
 
@@ -22,18 +26,36 @@ class Contender{
         clearInterval(this.postionTimer);
     }
 
+    recieveAstrick(striker){
+        let points = bodyPositionPoints[striker.position] - bodyPositionPoints[this.position];
+        this.updateScore(points);
+        striker.updateScore(points*-1);
+    }
+
+    updateScore(points){
+        this.lifeMetter = (this.lifeMetter - points);
+        console.log(this.type + ' lifeMetter = ' + this.lifeMetter);
+    }
+
 }
 
-let myContender = new Contender("");
+let aiContender = new Contender("Cat");
+let myContender = new Contender("Doge");
 
 function initTheGame(){
-    myContender.setType = "Doge";
+    // aiContender.setType = "Doge";
+    // myContender.setType = "Cat";
 }
 
 function stopTheGame(){
+    aiContender.gameOver();
     myContender.gameOver();
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
+
+function myStrick(){
+    aiContender.recieveAstrick(myContender);
+}
