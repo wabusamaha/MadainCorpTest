b8095dfbcef9b2d6117f5105ef6f3781558e7037
wabusamaha <52813165+wabusamaha@users.noreply.github.com>
2021-07-22 16:05:12 +0300

    I've a bug since the position from both objects are returning equal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,11 @@ class Contender{
         this.type = type; 
         this.lifeMetter = 100;
         this.position = getRandomInt(3);
-        this.postionTimer = setInterval(this.changePosition, 1000);
+        this.postionExchangeInterval = setInterval(this.changePosition, 1000);
+    }
+
+    setContender(myContender){
+        this.myContender = myContender;
     }
 
     setType(type){
@@ -19,26 +23,26 @@ class Contender{
     //    console.log(bodyPosition[this.position]);
        console.log(this.type + ' position is "' + bodyPosition[this.position] + '"');
 
-    //    this.postionTimer = setTimeout(this.changePosition, 1000);
+    //    this.postionExchangeInterval = setTimeout(this.changePosition, 1000);
     }
 
     gameOver(){
-        clearInterval(this.postionTimer);
+        clearInterval(this.postionExchangeInterval);
     }
 
-    recieveAstrick(striker){
-        let points = bodyPositionPoints[striker.position] - bodyPositionPoints[this.position];
+    recieveAstrick(){
+        let points = bodyPositionPoints[this.myContender.position] - bodyPositionPoints[this.position];
         this.updateScore(points);
-        striker.updateScore(points*-1);
+        this.myContender.updateScore(points*-1);
         // check how wins
-        // if ((this.lifeMetter == 0) || (striker.updateScore == 0)) {
-        if (striker.lifeMetter == 0 || this.updateScore == 0) {
+        // if ((this.lifeMetter == 0) || (this.myContender.updateScore == 0)) {
+        if (this.myContender.lifeMetter == 0 || this.updateScore == 0) {
             this.gameOver();
-            striker.gameOver();
+            this.myContender.gameOver();
             if (this.lifeMetter = 100) {
                 console.log(this.type + ' wins!');
             } else {
-                console.log(striker.type + ' wins!');
+                console.log(this.myContender.type + ' wins!');
             }
         } 
     }
@@ -55,16 +59,39 @@ class Contender{
 
 }
 
-let aiContender = new Contender("Cat");
+class aiContender extends Contender {
+    strickInterval;
+
+    constructor(type){
+        debugger;
+      super(type); 
+      this.aiOn = false;
+    }
+
+    set aiStatus(staus = true){
+        debugger;
+        this.aiOn = staus;
+        if (this.aiOn === true) {
+            this.strickInterval = setInterval(this.Contender.recieveAstrick, 1000);
+        }
+        else {
+            clearInterval(this.strickInterval);
+        }
+    }
+}
+
+let myAiContender = new aiContender("Cat");
 let myContender = new Contender("Doge");
 
 function initTheGame(){
-    // aiContender.setType = "Doge";
-    // myContender.setType = "Cat";
+    myAiContender.myContender = myContender;
+    myContender.myContender = myAiContender;
+    //
+    // myAiContender.aiStatus(true);
 }
 
 function stopTheGame(){
-    aiContender.gameOver();
+    myAiContender.gameOver();
     myContender.gameOver();
 }
 
@@ -73,5 +100,5 @@ function getRandomInt(max) {
   }
 
 function myStrick(){
-    aiContender.recieveAstrick(myContender);
+    myAiContender.recieveAstrick();
 }
